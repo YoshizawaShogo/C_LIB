e60bc01b4981092240bee8428050a584cc54efe9
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-08-04 11:50:42 +0900

    プロトタイプ宣言時にも、仮引数名を入力することで、エディタの補助により関数呼び出しの際に仮引数が分かるようにした。

--- lib/src/my_sort.h
+++ lib/src/my_sort.h
@@ -1,15 +1,15 @@
 #ifndef _my_sort_h
 #define _my_sort_h
 
-int *generate_array(int, int);
-int check_if_success_sort(int *, int);
-void get_time_sort(void (*pf)(int *, int), int *, int);
-void swap(int *, int *);
-void reverse_array(int *, int);
-void display_array(int *, int);
+int *generate_array(int length, int seed);
+int check_if_success_sort(int *array, int length);
+void get_time_sort(void (*pf)(int *, int), int *array, int length);
+void swap(int *a, int *b);
+void reverse_array(int *array, int length);
+void display_array(int *array, int length);
 
-void bubble_sort(int *, int);
-void quick_sort(int *, int);
-void merge_sort(int *, int);
+void bubble_sort(int *array, int length);
+void quick_sort(int *array, int length);
+void merge_sort(int *array, int length);
 
 #endif
